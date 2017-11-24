8e507f3ab866a483e3b769d410f0756f0ddffb04
sorgelig <pour.garbage@gmail.com>
2017-11-25 02:38:56 +0800

    Load ROM from SD

---
+++ releases/build_rom_BlackHole.bat
@@ -0,0 +1,40 @@
+@echo off
+
+set    zip=blkhole.zip
+set ifiles=bh1+bh2+bh3+bh4+bh5+bh6+bh6+bh6+bh7+bh7+bh8+bh8 
+set  ofile=a.blckhl.rom
+
+rem =====================================
+setlocal ENABLEDELAYEDEXPANSION
+
+set pwd=%~dp0
+echo.
+echo.
+
+if EXIST %zip% (
+
+	!pwd!7za x -otmp %zip%
+	if !ERRORLEVEL! EQU 0 ( 
+		cd tmp
+
+		copy /b/y %ifiles% !pwd!%ofile%
+		if !ERRORLEVEL! EQU 0 ( 
+			echo.
+			echo ** done **
+			echo.
+			echo Copy "%ofile%" into root of SD card
+		)
+		cd !pwd!
+		rmdir /s /q tmp
+	)
+
+) else (
+
+	echo Error: Cannot find "%zip%" file
+	echo.
+	echo Put "%zip%", "7za.exe" and "%~nx0" into the same directory
+)
+
+echo.
+echo.
+pause
